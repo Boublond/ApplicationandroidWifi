39a494a21dd301c4c86ebeadd8275d586b87f38d
Boublond <Alex@ndr3>
2015-06-30 14:25:27 +0100

    premiere ebauche de la classe wifi adapter

--- app/src/main/java/com/example/alexandrevey/applicationandroidwifi/WifiAdapter.java
+++ app/src/main/java/com/example/alexandrevey/applicationandroidwifi/WifiAdapter.java
@@ -1,6 +1,7 @@
 package com.example.alexandrevey.applicationandroidwifi;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,7 +43,30 @@ public class WifiAdapter extends BaseAdapter {
         ViewWifiHolder viewHolder;
         if(convertView== null){
             viewHolder = new ViewWifiHolder();
-            convertView = layoutInflater.inflate(R.layout.abc_list_menu_item_layout, null);
+            convertView = layoutInflater.inflate(R.layout.abc_list_menu_item_layout, null);//TODO modifier le type de la liste ?
+
+            viewHolder.tvApName= (TextView) convertView.findViewById(R.id.withText); //TODO modifier les types de R.ID
+            viewHolder.tvAdresseMAc= (TextView) convertView.findViewById(R.id.withText);
+            viewHolder.ForceSignal= (TextView) convertView.findViewById(R.id.withText);
+        } else {
+            viewHolder = (ViewWifiHolder)convertView.getTag();
         }
+
+        viewHolder.tvApName.setText(listeWifiItem.get(position).getAPName());
+        viewHolder.tvAdresseMAc.setText(listeWifiItem.get(position).getAdresseMac());
+
+
+        //Truc stylé qui devrait changer la couleur du Wifi en fonction de la puissance de son signal !
+        if(listeWifiItem.get(position).getForceSignal() <= -80) {
+            viewHolder.ForceSignal.setBackgroundColor(Color.GREEN);
+        } else if(listeWifiItem.get(position).getForceSignal() <= -50) {
+            viewHolder.ForceSignal.setBackgroundColor(Color.YELLOW);
+        } else {
+            viewHolder.ForceSignal.setBackgroundColor(Color.RED);
+        }
+
+        return convertView;
     }
+
 }
+
