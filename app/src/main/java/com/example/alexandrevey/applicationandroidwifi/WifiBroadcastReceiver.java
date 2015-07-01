a93cea57e96d9cdf90eb53e0b6405213676c958d
Boublond <Alex@ndr3>
2015-07-01 12:15:40 +0100

    Creation de la classe wifibroadcastreceiver

---
+++ app/src/main/java/com/example/alexandrevey/applicationandroidwifi/WifiBroadcastReceiver.java
@@ -0,0 +1,50 @@
+package com.example.alexandrevey.applicationandroidwifi;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alexandrevey on 01/07/15.
+ */
+public class WifiBroadcastReceiver extends BroadcastReceiver {
+    private WifiManager wifiManager;
+    private WifiAdapter wifiAdapter;
+    private List<WifiItem> listeWifiItem;
+    public void onReceive (Context context, Intent intent) {
+        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        // Gestion de la liste des AP WiFi (voir tuto sur les adapters et les
+        // listviews)
+        listeWifiItem = new ArrayList<WifiItem>();
+        wifiAdapter = new WifiAdapter(context, listeWifiItem);
+
+        if(wifiManager != null) {
+            if(wifiManager.isWifiEnabled()) {
+                List<ScanResult> listeScan = wifiManager.getScanResults();
+                listeWifiItem.clear();
+                for (ScanResult scanResult : listeScan) {
+                    WifiItem item = new WifiItem();
+                    item.setAdresseMac(scanResult.BSSID);
+                    item.setAPName(scanResult.SSID);
+                    item.setForceSignal(scanResult.level);
+
+                    listeWifiItem.add(item);
+                }
+                //TODO faire une notification pour rafraichir la vue
+            } else {
+                Toast.makeText(context,"You must enabled your wifi ", Toast.LENGTH_SHORT);
+            }
+
+        }
+
+    }
+
+
+}
